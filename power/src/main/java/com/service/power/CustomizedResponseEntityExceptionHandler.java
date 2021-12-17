db878fc8fbb38bfc4755fefbad5a7d972f473c8f
HenryTy <33482377+HenryTy@users.noreply.github.com>
2021-12-17 09:35:12 +0100

    Rest service is ready

---
+++ power/src/main/java/com/service/power/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.service.power;
+
+import java.util.Date;
+
+import com.service.power.model.RequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RequestException.class)
+    public final ResponseEntity<ExceptionResponse> handlePaymentException(RequestException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
--- power/src/main/java/com/service/power/PowerService.java
+++ power/src/main/java/com/service/power/PowerService.java
@@ -2,6 +2,8 @@ package com.service.power;
 
 import com.service.power.model.PowerRequest;
 import com.service.power.model.PowerResponse;
+import com.service.power.model.RequestException;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,31 +15,28 @@ import java.util.Random;
 @RestController
 public class PowerService {
 
+    @PostMapping("/service/power")
     public PowerResponse getPowers(@RequestBody PowerRequest powerRequest){
+        if(powerRequest.getRoomNr() > 10 || powerRequest.getRoomNr() < 1) {
+            throw new RequestException("Room Number must be value from 1 to 10");
+        }
         LocalDateTime from = powerRequest.getFrom();
         LocalDateTime to = powerRequest.getTo();
-        List<XMLGregorianCalendar> dates = new ArrayList<>();
-        List<Double> temperatures = new ArrayList<>();
-        XMLGregorianCalendar currentDate = (XMLGregorianCalendar) from.clone();
-        Duration minuteDuration = null;
-        try {
-            minuteDuration = DatatypeFactory.newInstance().newDuration("PT1M");
-        } catch (DatatypeConfigurationException e) {
-            e.printStackTrace();
-        }
+        List<LocalDateTime> dates = new ArrayList<>();
+        List<Double> powers = new ArrayList<>();
+        LocalDateTime currentDate = from;
         Random random = new Random();
-        double minTemp = 16.0;
-        double maxTemp = 24.0;
-        while(currentDate.compare(to) <= 0) {
+        double minPower = 800;
+        double maxPower = 1500;
+        while(currentDate.compareTo(to) <= 0) {
             dates.add(currentDate);
-            double temp = minTemp + (maxTemp - minTemp) * random.nextDouble();
-            temperatures.add(temp);
-            currentDate = (XMLGregorianCalendar) currentDate.clone();
-            currentDate.add(minuteDuration);
+            double temp = minPower + (maxPower - minPower) * random.nextDouble();
+            powers.add(temp);
+            currentDate = currentDate.plusMinutes(1);
         }
-        TemperatureResponse temperatureResponse = new TemperatureResponse();
-        temperatureResponse.setDates(dates);
-        temperatureResponse.setTemperatures(temperatures);
-        return temperatureResponse;
+        PowerResponse powerResponse = new PowerResponse();
+        powerResponse.setDates(dates);
+        powerResponse.setPowers(powers);
+        return powerResponse;
     }
 }
